8294dc342cbc64aa810a59fb48bad3b4746547b8
트립비토즈 김수영 <ksy9926@naver.com>
2022-01-25 23:51:36 +0900

    aws new instance test

--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -16,9 +16,10 @@ export const VIDEO_TYPE = {
 
 // API 주소 목록
 
-export const API = 'http://ec2-15-164-214-76.ap-northeast-2.compute.amazonaws.com:8080'
+export const API = 'http://ec2-3-36-85-61.ap-northeast-2.compute.amazonaws.com:8080'
 // export const API = 'http://localhost:8080';
-export const STUDIO_API='http://ec2-15-164-214-76.ap-northeast-2.compute.amazonaws.com:3000/api/odin'
+export const STUDIO_API='http://ec2-3-36-85-61.ap-northeast-2.compute.amazonaws.com:3000/api/odin'
+// export const STUDIO_API='http://localhost:3000/api/odin'
 
 // 어드민 수정 플래그
 export const MODIFY_FLAG = {
